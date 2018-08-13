80dc02048423e8c8e9f856a22ea01cdeca04690c
Ann Caly <anncaly@gmail.com>
2018-08-14 01:02:17 +0300

    change tabs to spaces

--- src/user.js
+++ src/user.js
@@ -1,12 +1,12 @@
 import './user.scss';
 
 export default class User {
-	constructor(firstName, lastName) {
-		this.firstName = firstName;
-		this.lastName = lastName;
-	}
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
 
-	sayHi() {
-		return `Hello, ${this.firstName} ${this.lastName}`
-	}
-}	
+  sayHi() {
+    return `Hello, ${this.firstName} ${this.lastName}`
+  }
+} 
--- webpack.config.js
+++ webpack.config.js
@@ -4,39 +4,39 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-	entry: {
-		main: path.resolve(__dirname, 'src', 'index.js')
-	},
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'bundle.[chunkhash].js',
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js/,
-				exclude: /node_modules/,
-				loader: 'babel-loader'
-			},
-			{
+  entry: {
+    main: path.resolve(__dirname, 'src', 'index.js')
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.[chunkhash].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
         test: /\.s?[ac]ss$/,
         use: [
-        	MiniCssExtractPlugin.loader, 
-        	'css-loader', 
-        	'sass-loader', 
-        	'postcss-loader'
+          MiniCssExtractPlugin.loader, 
+          'css-loader', 
+          'sass-loader', 
+          'postcss-loader'
         ],
-    	},
+      },
     ]
-	},
-	plugins: [
-		new MiniCssExtractPlugin({
-			filename: "[name].[chunkhash].css",
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].[chunkhash].css",
       //chunkFilename: "[id].css"
     }),
-		new HtmlWebpackPlugin({
-			template: path.resolve(__dirname, 'src', 'index.html'),
-			filename: 'index.html',
-		})
-	]
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html'),
+      filename: 'index.html',
+    })
+  ]
 }
